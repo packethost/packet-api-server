b1e568b391b236f514dec4ce236ca1900f2ec6b3
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2023-02-22 15:14:59 -0600

    Remove all volume related functions
    Signed-off-by: Chris Privitere <23177737+cprivitere@users.noreply.github.com>

--- pkg/store/memory.go
+++ pkg/store/memory.go
@@ -2,39 +2,26 @@ package store
 
 import (
 	"fmt"
-	"sort"
 
 	"github.com/google/uuid"
-	"github.com/packethost/packet-api-server/pkg/util"
 	"github.com/packethost/packngo"
-)
-
-const (
-	// we use constants for these, since in memory, it does not really matter
-	iqn = "iqn.2013-05.com.daterainc:tc:01:sn:73d3e29022fddba4"
-	ip1 = "10.144.32.8"
-	ip2 = "10.144.48.8"
 )
 
 // Memory is an implementation of DataStore which stores everything in memory
 type Memory struct {
-	volumes     map[string]*packngo.Volume
-	attachments map[string]*packngo.VolumeAttachment
-	facilities  map[string]*packngo.Facility
-	devices     map[string]*packngo.Device
-	plans       map[string]*packngo.Plan
-	bgp         map[string]*packngo.BGPConfig
+	facilities map[string]*packngo.Facility
+	devices    map[string]*packngo.Device
+	plans      map[string]*packngo.Plan
+	bgp        map[string]*packngo.BGPConfig
 }
 
 // NewMemory returns a properly initialized Memory
 func NewMemory() *Memory {
 	return &Memory{
-		volumes:     map[string]*packngo.Volume{},
-		attachments: map[string]*packngo.VolumeAttachment{},
-		facilities:  map[string]*packngo.Facility{},
-		devices:     map[string]*packngo.Device{},
-		plans:       map[string]*packngo.Plan{},
-		bgp:         map[string]*packngo.BGPConfig{},
+		facilities: map[string]*packngo.Facility{},
+		devices:    map[string]*packngo.Device{},
+		plans:      map[string]*packngo.Plan{},
+		bgp:        map[string]*packngo.BGPConfig{},
 	}
 }
 
@@ -176,127 +163,6 @@ func (m *Memory) DeleteDevice(deviceID string) (bool, error) {
 	return false, nil
 }
 
-// ListVolumes list the volumes for the project
-func (m *Memory) ListVolumes(projectID string, listOpt *packngo.ListOptions) ([]*packngo.Volume, error) {
-	start, end := calculateStartEndSlice(len(m.volumes), listOpt)
-
-	vols := make([]*packngo.Volume, 0, end-start)
-	// we sort consistently by lexical order of volume ID. Just because.
-	var keys []string
-	for k := range m.volumes {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	for _, k := range keys[start:end] {
-		vols = append(vols, m.volumes[k])
-	}
-	return vols, nil
-}
-
-// GetVolume get information about a single volume
-func (m *Memory) GetVolume(volID string) (*packngo.Volume, error) {
-	if vol, ok := m.volumes[volID]; ok {
-		return vol, nil
-	}
-	return nil, nil
-}
-
-// DeleteVolume delete a single volume
-func (m *Memory) DeleteVolume(volID string) (bool, error) {
-	if _, ok := m.volumes[volID]; ok {
-		delete(m.volumes, volID)
-		return true, nil
-	}
-	return false, nil
-}
-
-// CreateVolume create a new volume
-func (m *Memory) CreateVolume(cvr packngo.VolumeCreateRequest) (*packngo.Volume, error) {
-	// just create it
-	uuid := uuid.New().String()
-	vol := &packngo.Volume{
-		ID:          uuid,
-		Name:        util.VolumeIDToName(uuid),
-		Description: cvr.Description,
-		Size:        cvr.Size,
-		State:       "active",
-		Plan:        &packngo.Plan{ID: cvr.PlanID},
-	}
-	m.volumes[uuid] = vol
-	return vol, nil
-}
-
-// AttachVolume attach a volume to a device
-func (m *Memory) AttachVolume(volID string, deviceID string) (*packngo.VolumeAttachment, error) {
-	var (
-		vol *packngo.Volume
-		dev *packngo.Device
-		ok  bool
-	)
-	// make sure we have the volume and dveice
-	if vol, ok = m.volumes[volID]; !ok {
-		return nil, nil
-	}
-	if dev, ok = m.devices[deviceID]; !ok {
-		return nil, nil
-	}
-	uuid := uuid.New().String()
-	attachment := packngo.VolumeAttachment{
-		ID:     uuid,
-		Device: packngo.Device{ID: deviceID},
-		Volume: *vol,
-	}
-	vol.Attachments = append(vol.Attachments, &attachment)
-	dev.Volumes = append(dev.Volumes, vol)
-	m.attachments[uuid] = &attachment
-	return &attachment, nil
-}
-
-// DetachVolume detach a volume from a device
-func (m *Memory) DetachVolume(attachID string) (bool, error) {
-	var (
-		attachment *packngo.VolumeAttachment
-		ok         bool
-	)
-	if attachment, ok = m.attachments[attachID]; !ok {
-		return false, nil
-	}
-	devID := attachment.Device.ID
-	volID := attachment.Volume.ID
-	// remove the attachment from the volume
-	if vol, ok := m.volumes[volID]; ok {
-		n := 0
-		for _, x := range vol.Attachments {
-			if x.ID != attachID {
-				vol.Attachments[n] = x
-				n++
-			}
-		}
-		vol.Attachments = vol.Attachments[:n]
-	}
-	// remove the volume from the device
-	if dev, ok := m.devices[devID]; ok {
-		n := 0
-		for _, x := range dev.Volumes {
-			if x.ID != volID {
-				dev.Volumes[n] = x
-				n++
-			}
-		}
-		dev.Volumes = dev.Volumes[:n]
-	}
-
-	// delete the attachment
-	delete(m.attachments, attachID)
-
-	return true, nil
-}
-
-// GetAttachmentMetadata get the metadata about a given attachment
-func (m *Memory) GetAttachmentMetadata(attachID string) (string, []string, error) {
-	return iqn, []string{ip1, ip2}, nil
-}
-
 func (m *Memory) EnableBGP(projectID string, cbgpcr packngo.CreateBGPConfigRequest) error {
 	m.bgp[projectID] = &packngo.BGPConfig{
 		ID:             uuid.New().String(),
@@ -304,28 +170,4 @@ func (m *Memory) EnableBGP(projectID string, cbgpcr packngo.CreateBGPConfigReque
 		Asn:            cbgpcr.Asn,
 	}
 	return nil
-}
-
-func calculateStartEndSlice(total int, listOpt *packngo.ListOptions) (int, int) {
-	count := total
-	start := 0
-	if listOpt != nil && listOpt.PerPage > 0 {
-		count = listOpt.PerPage
-	}
-	if listOpt != nil {
-		start = listOpt.Page
-	}
-	// if we asked to start past the end, start at the last
-	if start >= total {
-		start = total - 1
-	}
-	if start < 0 {
-		start = 0
-	}
-	end := start + count
-	// if we asked to finish past the end, finish at the last
-	if end > total {
-		end = total
-	}
-	return start, end
 }
